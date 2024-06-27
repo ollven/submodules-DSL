bcad52e1b3b3bb4450157a99a19d774cb0c28fde
ollven <ollven@TeamCity>
2024-06-27 14:39:23 +0200

    TeamCity change in 'Submodules' project: Synchronization with own VCS root is enabled

---
+++ .teamcity/settings.kts
@@ -0,0 +1,89 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.vcs.GitVcsRoot
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2024.03"
+
+project {
+
+    vcsRoot(HttpsGithubComOllvenSubmodulesRefsHeadsMain)
+
+    buildType(Child1)
+    buildType(Build)
+}
+
+object Build : BuildType({
+    name = "Build"
+
+    vcs {
+        root(HttpsGithubComOllvenSubmodulesRefsHeadsMain)
+
+        cleanCheckout = true
+    }
+
+    steps {
+        script {
+            name = "Tree"
+            id = "Tree"
+            scriptContent = """
+                tree
+                cat README.md
+                cat level1-2/README.md
+            """.trimIndent()
+        }
+    }
+
+    triggers {
+        vcs {
+        }
+    }
+
+    features {
+        perfmon {
+        }
+    }
+})
+
+object Child1 : BuildType({
+    name = "child1"
+
+    vcs {
+        root(HttpsGithubComOllvenSubmodulesRefsHeadsMain)
+    }
+})
+
+object HttpsGithubComOllvenSubmodulesRefsHeadsMain : GitVcsRoot({
+    name = "https://github.com/ollven/Submodules#refs/heads/main"
+    url = "https://github.com/ollven/Submodules"
+    branch = "refs/heads/main"
+    branchSpec = "refs/heads/*"
+    userForTags = "ollven<ollven@yandex.ru>"
+    authMethod = password {
+        userName = "ollven"
+        password = "credentialsJSON:9fd10c2d-c48f-40aa-9156-19acfe23cab2"
+    }
+})
